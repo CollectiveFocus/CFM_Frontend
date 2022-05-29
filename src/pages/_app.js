655222f0be142a6fac50e896c9a1d458e10a66b6
Bernard Martis <martisbvk@gmail.com>
2022-05-28 23:39:06 -0400

    feat: Setup MUI (ui), Emotion (js-to-css)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,36 @@
-import Layout from 'components/Layout'
-import 'assets/css/global.css'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { CacheProvider } from '@emotion/react'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
+import createEmotionCache from 'lib/createEmotionCache'
+import theme from 'theme'
+
+const clientSideEmotionCache = createEmotionCache()
+
+const MyApp = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <React.Fragment>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </React.Fragment>
   )
 }
 
 export default MyApp
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired
+}
