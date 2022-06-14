ce5e0be364d17de0365017950817baaae7c5bfff
ioanat94 <ioanatiplea94@gmail.com>
2022-06-14 09:11:39 +0300

    feat: Create Backtrack component

---
+++ src/components/Backtrack.jsx
@@ -0,0 +1,42 @@
+import { useRouter } from 'next/router';
+import { Box, Container, Typography } from '@mui/material';
+import { themeOptions } from '../theme/index';
+import { ArrowBack } from '@mui/icons-material';
+
+export default function Backtrack() {
+  const router = useRouter();
+  const { asPath } = useRouter();
+
+  const prevLocation =
+    asPath.includes('update') || asPath.includes('checkin') ? 'Fridge' : 'Map';
+
+  return (
+    <Box>
+      <Container
+        fixed
+        disableGutters
+        maxWidth={false}
+        onClick={() => router.back()}
+        sx={{
+          minWidth: '100%',
+          height: '54px',
+          paddingLeft: '17px',
+          backgroundColor: '#f6f6f6',
+          display: 'flex',
+          alignItems: 'center',
+          color: themeOptions.palette.text.secondary,
+          ':hover': { cursor: 'pointer' },
+        }}
+      >
+        <ArrowBack fontSize="medium" />
+        <Typography
+          sx={{
+            fontSize: '15px',
+          }}
+        >
+          {`Back to ${prevLocation}`}
+        </Typography>
+      </Container>
+    </Box>
+  );
+}
