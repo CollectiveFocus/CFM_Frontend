430a130ad2652139b2217b3a8fb10aadad65f287
Bernard Martis <martisbvk@gmail.com>
2022-06-20 07:58:06 -0400

    chore: Create settings for importing svg

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-};
 
-module.exports = nextConfig;
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
