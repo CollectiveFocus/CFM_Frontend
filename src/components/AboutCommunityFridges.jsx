ab56205008cfb891408f897e7de0690026908fc6
ScottLy426 <lyscott123@gmail.com>
2022-06-21 10:48:45 -0700

    refactor: Changed Grid component to Box component Switched to Box MUI component to better utilize grid layout for mobile and desktop design

--- src/components/AboutCommunityFridges.jsx
+++ src/components/AboutCommunityFridges.jsx
@@ -3,39 +3,99 @@ import PropTypes from 'prop-types';
 import Image from 'next/image';
 import Link from 'next/link';
 import PearFridgeTomato from '../../public/mascot/pearFridgeTomatos.svg';
-import { Typography, Stack, Button, Divider } from '@mui/material';
+import { Typography, Button, Box, Item } from '@mui/material';
 
 export default function AboutCommunityFridges() {
   return (
-    <Stack
-      container
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      direction="column"
-      spacing={4}
-      borderTop={`1px solid #B4B4B4`}
-      borderBottom={`1px solid #B4B4B4`}
-      paddingTop="40px"
-      paddingBottom="40px"
-      maxWidth="90%"
-      marginInline="auto"
+    <Box
+      sx={{
+        display: 'grid',
+        gap: 4,
+        gridTemplateColumns: {
+          xs: '1fr',
+          md: '1fr 1fr',
+        },
+        gridTemplateRows: {
+          xs: '1fr',
+          m: '1fr',
+        },
+        placeItems: {
+          xs: 'center',
+          md: 'start start',
+        },
+        borderBlock: '1px solid #B4B4B4',
+        paddingBlock: '40px',
+      }}
     >
-      <Typography variant="h2" textAlign="center" sx={{ mb: { xs: 3, lg: 5 } }}>
-        About NYC Community Fridges
-      </Typography>
-      <Image src={PearFridgeTomato} alt="pear fridge tomato" />
-      <Typography variant="body1" textAlign="center">
-        A community fridge is a decentralized food resource. There are dozens of
-        fridges hosted by volunteers across the New York Cty area. This website
-        was made to make it easy for people to find fridges and get involved
-        with the community fridge project.
-      </Typography>
-      <Link href={'/about'} passHref>
-        <Button component="a" fullWidth={true}>
-          LEARN MORE
-        </Button>
-      </Link>
-    </Stack>
+      <Box
+        sx={{
+          order: {
+            md: 0,
+          },
+          alignSelf: {
+            md: 'end',
+          },
+        }}
+      >
+        <Typography variant="h2" textAlign="center">
+          About NYC Community Fridges
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          gridRow: {
+            md: 'span 3',
+          },
+          order: {
+            md: -1,
+          },
+          alignSelf: {
+            md: 'center',
+          },
+          justifySelf: {
+            md: 'center',
+          },
+        }}
+      >
+        <Image src={PearFridgeTomato} alt="pear fridge tomato" />
+      </Box>
+      <Box
+        sx={{
+          fontSize: {
+            xs: '12px',
+            md: '12px',
+          },
+        }}
+      >
+        <Typography
+          variant="body1"
+          textAlign="center"
+          sx={{ textAlign: { xs: 'center', md: 'left' } }}
+        >
+          A community fridge is a decentralized food resource. There are dozens
+          of fridges hosted by volunteers across the New York Cty area. This
+          website was made to make it easy for people to find fridges and get
+          involved with the community fridge project.
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          width: {
+            xs: '100vw',
+            md: 'max-content',
+          },
+        }}
+      >
+        <Link href={'/about'} passHref>
+          <Button
+            component="a"
+            fullWidth={true}
+            sx={{ paddingInline: { md: '100px' } }}
+          >
+            LEARN MORE
+          </Button>
+        </Link>
+      </Box>
+    </Box>
   );
 }
