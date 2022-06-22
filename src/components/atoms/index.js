7fc7d180d7eb5da55f857d43394fb23e6243c27b
Andrew R <abstractdev0@gmail.com>
2022-06-22 00:34:09 -0400

    feat: Create ButtonLink component

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,3 +1,4 @@
+export { default as ButtonLink } from './ButtonLink';
 export { default as NextLinkAnchor } from './NextLinkAnchor';
 export { default as SuccessMessage } from './SuccessMessage';
 export { default as ThemeIcon } from './ThemeIcon';
