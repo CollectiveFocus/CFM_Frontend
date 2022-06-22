73439026cc5af40c6d55a1108004ac96f8154ee8
Andrew R <abstractdev0@gmail.com>
2022-06-22 00:34:09 -0400

    feat: Create ButtonLink component

---
+++ src/components/molecules/ButtonLink/ButtonLink.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import { Button } from '@mui/material';
+import { NextLinkAnchor } from 'components/atoms';
+
+export default function ButtonLink(props) {
+  return <Button component={NextLinkAnchor} {...props} />;
+}
+
+const toPathname = PropTypes.shape({
+  pathname: PropTypes.string.isRequired,
+  query: PropTypes.object.isRequired,
+});
+
+ButtonLink.propTypes = {
+  /**
+   * The URL or pathname object to navigate to.
+   *
+   * to='/about'
+   * to={{ pathname: '/blog/[slug]', query: { slug: post.slug }, }}
+   */
+  to: PropTypes.oneOfType([PropTypes.string, toPathname]).isRequired,
+
+  /**
+   * Text describing the content of the link.
+   * eg: aria-label='Learn more about the community fridge map project.'
+   */
+  ariaLabel: PropTypes.string.isRequired,
+
+  /***
+   * Style of button from theme
+   */
+  variant: PropTypes.oneOf(['filled', 'contained']).isRequired,
+};
