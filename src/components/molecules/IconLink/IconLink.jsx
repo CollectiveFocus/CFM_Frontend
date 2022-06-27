e16f88ef167d8f6e3a67bd8c34b8a392634af2a5
Andrew R <abstractdev0@gmail.com>
2022-06-26 21:02:23 -0400

    feat: Create IconLink component

---
+++ src/components/molecules/IconLink/IconLink.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { IconButton } from '@mui/material';
+import { NextLinkAnchor } from 'components/atoms';
+
+export default function IconLink(props) {
+  return <IconButton component={NextLinkAnchor} {...props} />;
+}
+
+const toPathname = PropTypes.shape({
+  pathname: PropTypes.string.isRequired,
+  query: PropTypes.object.isRequired,
+});
+
+IconLink.propTypes = {
+  /**
+   * The URL or pathname object to navigate to.
+   *
+   * to='/about'
+   * to={{ pathname: '/blog/[slug]', query: { slug: post.slug }, }}
+   */
+  to: PropTypes.oneOfType([PropTypes.string, toPathname]).isRequired,
+
+  /**
+   * Text describing the content of the link.
+   * eg: aria-label='Learn more about the community fridge map project.'
+   */
+  ariaLabel: PropTypes.string.isRequired,
+};
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,2 +1,3 @@
 export { default as Backtrack } from './Backtrack';
 export { default as ButtonLink } from './ButtonLink';
+export { default as IconLink } from './IconLink';
