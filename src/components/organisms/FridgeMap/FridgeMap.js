b155341fc27414343458aa85524ceb81570790e1
Weston Norwood <weston.norwood@gmail.com>
2022-07-06 23:29:57 -0700

    chore: Trying various methods of including svg

--- src/components/organisms/FridgeMap/FridgeMap.js
+++ src/components/organisms/FridgeMap/FridgeMap.js
@@ -1,21 +1,49 @@
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import { Container } from '@mui/system';
+import Leaflet from 'leaflet';
+import { Button } from '@mui/material';
+import { MapPinIcon } from 'theme/icons';
 
 const FridgeMap = () => {
   const fridgeData = require('./FridgeData.json');
   const defaultLocation = [40.70580857568261, -73.99646699561376]; // default location Brooklyn Bridge
 
-  let basePin = require('../../../theme/icons/');
-  let svgURL = 'data:image/svg+xml;base64,' + btoa(icon);
+  let basePin = require('../../../theme/icons/mapPin.svg');
+  let svgUrl = 'data:image/svg+xml;base64,' + btoa(basePin);
 
   const fridgePin = new Leaflet.Icon({
-    iconUrl: svgURL,
+    iconUrl: require('../../../theme/icons/mapPin.svg'),
+    iconRetinaUrl: require('../../../theme/icons/mapPin.svg'),
+    shadowUrl: null,
     iconAnchor: [21, 43],
     popupAnchor: [10, -44],
     iconSize: [43, 43],
   });
+  // If I place <MapPinIcon> Directly in return it will render. does not like being child of <Marker> probably because this is JSX?
+  const fridgeDiv = Leaflet.divIcon({
+    className: 'fridge-pin',
+    html: `
+    <MapPinIcon sx={{zIndex:999}}>
+    Test
+    </MapPinIcon>
+    `,
+  });
+
+  // const fridgeDiv = Leaflet.divIcon({
+  //   className: 'fridge-pin',
+  //   html: `
+  //   <div>
+  //   {basePin}
+  //   <MapPinIcon sx={{zIndex:999}}>
+  //   Test
+  // </MapPinIcon>
+  //     </div>
+  //   `,
+  // });
+
   return (
     <Container sx={{ padding: 0, margin: 0 }}>
+      {/* <MapPinIcon sx={{}}>Test</MapPinIcon> //This will render */}
       <MapContainer
         style={{ height: '100vh' }}
         center={defaultLocation}
@@ -28,6 +56,7 @@ const FridgeMap = () => {
           maxZoom={19}
           subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
         />
+        <Marker icon={fridgeDiv} position={defaultLocation}></Marker>
       </MapContainer>
     </Container>
   );
