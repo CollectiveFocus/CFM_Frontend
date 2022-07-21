455ce1d390f49a3cfe2f389bc4e2ef30089614fd
Bernard Martis <martisbvk@gmail.com>
2022-07-20 20:14:04 -0400

    wip: Yuo schema

--- src/schema/cfm/yup/index.js
+++ src/schema/cfm/yup/index.js
@@ -1,16 +1,8 @@
-import { object, string, oneOf, array } from 'Yup';
+import { object, string, array, number, boolean } from 'Yup';
 
 const Tag = string().max(140).trim().required();
+const Tags = array().of(Tag).nullable();
 
-const Fridge = object().shape({
-  id: string().min(5).max(60).required(),
-  name: string().min(5).max(60).trim().required(),
-  location: Location.required(),
-  tags: array().of(Tag).optional(),
-  maintainer: Maintainer.optional(),
-  notes: string().min(0).max(300).trim().default('').optional(),
-  photoURL: string().url().optional(),
-});
 const Location = object().shape({
   street: string().max(55).trim().required(),
   city: string().max(35).trim().required(),
@@ -19,21 +11,41 @@ const Location = object().shape({
     .matches(/(^\d{5}$)|(^\d{5}-\d{4}$)/)
     .required(),
 });
-const Maintainer = object().shape({
-  name: string().max(70).trim().required(),
-  email: string().email().required(),
-  organization: string().max(80).trim().optional(),
-  phone: string()
-    .matches(/\(\d{3}\) \d{3}-\d{4}/)
-    .optional(),
-  website: string().url().optional(),
-  instagram: string().url().optional(),
-});
-const Report = object().shape({
-  condition: oneOf(['dirty', 'out of order', 'not at location']).nullable(),
-  foodPercentage: oneOf([0, 25, 50, 75, 100]),
-  foodPhotoURL: string().url().optional(),
-  notes: string().min(0).max(300).trim().default('').optional(),
-});
 
-export { Fridge as FridgeDialogSchema, Report as ReportDialogSchema };
+const Maintainer = object()
+  .nullable()
+  .shape({
+    name: string().max(70).trim().required(),
+    email: string().email().required(),
+    organization: string().max(80).trim().optional(),
+    phone: string()
+      .matches(/\(\d{3}\) \d{3}-\d{4}/)
+      .optional(),
+    website: string().url().optional(),
+    instagram: string().url().optional(),
+  });
+
+const Report = object()
+  .nullable()
+  .shape({
+    condition: string()
+      .oneOf(['good', 'dirty', 'out of order', 'not at location'])
+      .optional(),
+    foodPercentage: number().integer().oneOf([0, 33, 66, 100]),
+    foodPhotoURL: string().url().optional(),
+    notes: string().min(0).max(300).trim().default('').optional(),
+  });
+
+const FridgeApiSchema = object().shape({
+  id: string().min(4).max(60).required(),
+  name: string().min(4).max(60).trim().required(),
+  location: Location.required(),
+  tags: Tags.default(null),
+  maintainer: Maintainer.default(null),
+  notes: string().min(0).max(300).trim().default(''),
+  photoURL: string().url().optional(),
+  verified: boolean().default(false),
+});
+const  FridgeDialogSchema = FridgeApiSchema.omit(['id', 'verified', 'tags']);
+
+export default { FridgeDialogSchema, ReportDialogSchema: Report };
