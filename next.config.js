2fa267236af7d9d2e6fd3d318e9f3254bf1e28f6
Bernard Martis <martisbvk@gmail.com>
2022-07-28 17:01:34 -0400

    perf: Reduce Next.js compile time

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 module.exports = {
   reactStrictMode: true,
 
+  // --- Next.js@12.2.2
+  swcMinify: true,
+  compiler: {
+    emotion: true,
+  },
+  experimental: {
+    modularizeImports: {
+      '@mui/material': {
+        transform: '@mui/material/{{member}}',
+      },
+      '@mui/icons-material': {
+        transform: '@mui/icons-material/{{member}}',
+      },
+      'components/atoms': {
+        transform: 'components/atoms/{{member}}',
+      },
+      'components/molecules': {
+        transform: 'components/molecules/{{member}}',
+      },
+      'components/organisms': {
+        transform: 'components/organisms/{{member}}',
+      },
+    },
+  },
+  // ---
+
   eslint: {
     dirs: ['src/', 'ci/'],
   },
