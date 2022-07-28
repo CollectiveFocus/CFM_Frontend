c9899bac49a16dd470ecd6c9863ca67f43befcae
Bernard Martis <martisbvk@gmail.com>
2022-07-28 16:18:04 -0400

    feat: Create components for theme icons

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,4 +1,3 @@
 export { default as NextLinkAnchor } from './NextLinkAnchor';
 export { default as PageFooter } from './PageFooter';
 export { default as SuccessMessage } from './SuccessMessage';
-export { default as ThemeIcon } from './ThemeIcon';
