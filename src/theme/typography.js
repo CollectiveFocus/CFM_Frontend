93113511994b74de8363c17a96671d88c1e1cfc0
Bernard Martis <martisbvk@gmail.com>
2022-07-28 15:55:41 -0400

    refactor: MUI theme and typography

---
+++ src/theme/typography.js
@@ -0,0 +1,24 @@
+const typography = {
+  fontFamily: [
+    'Inter',
+    '"Helvetica Neue"',
+    'HelveticaNeue',
+    'Helvetica',
+    '"TeX Gyre"',
+    'TeXGyre',
+    'Arial',
+    'sans-serif',
+  ].join(','),
+  h1: { fontSize: '36pt', fontWeight: 700 },
+  h2: { fontSize: '28pt', fontWeight: 700 },
+  h3: { fontSize: '28pt', fontWeight: 400 },
+  h4: { fontSize: '18pt', fontWeight: 700 },
+  h5: { fontSize: '18pt', fontWeight: 500 },
+  h6: { fontSize: '16pt', fontWeight: 500 },
+  body1: { fontSize: '18pt', fontWeight: 400 },
+  body2: { fontSize: '16pt', fontWeight: 400 },
+  button: { fontSize: '18pt', fontWeight: 700 },
+  caption: { fontSize: '15pt', fontWeight: 700, letterSpacing: 0.5 },
+};
+
+export default typography;
