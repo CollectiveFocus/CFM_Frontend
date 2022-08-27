83dbfdf53d2c04ffee53dc4c33877dfd20315edc
Weston Norwood <weston.norwood@gmail.com>
2022-08-26 17:14:13 -0700

    feat: Update AppBar component

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,3 +1,4 @@
+export { default as AppBar } from './AppBar';
 export { default as Backtrack } from './Backtrack';
 export { default as ButtonLink } from './ButtonLink';
 export { default as DialogUploadImage } from './DialogUploadImage';
--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,2 +1,1 @@
-export { default as AppBar } from './AppBar';
 export { default as DialogUpdateFridgeStatus } from './DialogUpdateFridgeStatus';
