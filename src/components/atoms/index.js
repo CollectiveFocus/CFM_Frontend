a33e9aa392b35d6e1f13b30c648b4614aee7c84a
Weston Norwood <weston.norwood@gmail.com>
2022-08-30 19:58:37 -0700

    feat: Create FeedbackCard component from FeedbackMessage

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,5 +1,5 @@
 export { default as DecoratedParagraph } from './DecoratedParagraph';
-export { default as FeedbackMessage } from './FeedbackMessage';
+export { default as FeedbackCard } from './FeedbackCard';
 export { default as NextLinkAnchor } from './NextLinkAnchor';
 export { default as PageFooter } from './PageFooter';
 export { default as PageHero } from './PageHero';
