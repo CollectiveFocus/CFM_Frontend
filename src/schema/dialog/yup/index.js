9ae33800d886b592b77f67181027cd211fbd42e7
Bernard Martis <martisbvk@gmail.com>
2022-09-02 03:44:24 -0400

    feat: Update schema for Dialog/Yup

---
+++ src/schema/dialog/yup/index.js
@@ -0,0 +1,24 @@
+import apiFridge from 'schema/api/fridge/yup';
+
+export const dialogContact = apiFridge.Contact;
+
+export const dialogLocation = apiFridge.Location.omit(['geoLat', 'geoLng']);
+
+export const dialogFridge = apiFridge.Fridge.omit([
+  'id',
+  'verified',
+  'tags',
+  'location',
+]).shape({
+  location: dialogLocation.required(),
+});
+
+export const dialogReport = apiFridge.Report.omit(['timestamp']);
+
+const dialogDataValidation = {
+  Contact: dialogContact,
+  Fridge: dialogFridge,
+  Location: dialogLocation,
+  Report: dialogReport,
+};
+export default dialogDataValidation;
