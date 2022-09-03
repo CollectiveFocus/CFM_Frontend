672d32ee74d341c02c5a8dfca6d53e98f491fb6c
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-09-02 19:24:24 -0700

    feat: Create TitleCard component

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -4,3 +4,4 @@ export { default as NextLinkAnchor } from './NextLinkAnchor';
 export { default as PageFooter } from './PageFooter';
 export { default as PageHero } from './PageHero';
 export { default as PageScroll } from './PageScroll';
+export { default as TitleCard } from './TitleCard';
