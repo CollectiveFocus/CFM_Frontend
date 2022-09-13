cf7bb9f420301873863c3ed6f0e966312ad1e557
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-09-12 22:03:00 -0700

    feat: Create GroupByDistance function

--- src/lib/search.js
+++ src/lib/search.js
@@ -1,6 +1,6 @@
 const rxPunctuationAndDigits = /[.,\/#!$%\^&\*;:{}=\-_`~()\'\d]/g;
 
-function WordRank(sentences, excludedWords) {
+export function WordRank(sentences, excludedWords) {
   const words = sentences
     .join(' ')
     .replaceAll(rxPunctuationAndDigits, '')
@@ -12,4 +12,19 @@ function WordRank(sentences, excludedWords) {
   const wordCounts = {};
   words.forEach((word) => (wordCounts[word] = 1 + (wordCounts[word] ?? 0)));
   return wordCounts;
+}
+
+export function GroupByDistance(delta, distanceList) {
+  const groups = {};
+  distanceList.forEach(({ fridgeId, distance }) => {
+    const myGroup = Math.floor(distance / delta);
+
+    if (groups[myGroup]) {
+      groups[myGroup] = groups[myGroup].concat(fridgeId);
+    } else {
+      groups[myGroup] = [fridgeId];
+    }
+  });
+
+  return Object.keys(groups).map((key) => groups[key]);
 }
