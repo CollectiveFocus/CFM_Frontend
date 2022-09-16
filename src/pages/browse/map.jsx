7dd05a1d30b1cc2791db908a7d12c8cc67d594d8
Weston Norwood <weston.norwood@gmail.com>
2022-09-15 21:06:55 -0700

    feat: Create Map page

--- src/pages/browse/map.jsx
+++ src/pages/browse/map.jsx
@@ -1,17 +1,28 @@
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import NoSsr from '@mui/material/NoSsr';
+import dynamic from 'next/dynamic';
 
-export async function getStaticProps() {
-  return { props: {} };
-}
+const locationBrooklynBridge = {
+  geoLat: 40.70580857568261,
+  geoLng: -73.99646699561376,
+};
 
 export default function BrowseMapPage() {
+  const DynamicMap = dynamic(
+    () => {
+      return import('../../components/organisms/map/LeafletMap');
+    },
+    { ssr: false }
+  );
   return (
     <>
       <Head>
         <title>CFM: Geographic Map</title>
       </Head>
-      <p>CFM: Geographic Map</p>
+      <NoSsr>
+        <DynamicMap {...locationBrooklynBridge} />
+      </NoSsr>
     </>
   );
 }
