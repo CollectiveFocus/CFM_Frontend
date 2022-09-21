84e462ca512209557b803710334248f19f5db510
Bernard Martis <martisbvk@gmail.com>
2022-09-21 01:16:10 -0400

    feat: Create Browse page

--- src/pages/browse.jsx
+++ src/pages/browse.jsx
@@ -1,28 +1,55 @@
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import NoSsr from '@mui/material/NoSsr';
 import dynamic from 'next/dynamic';
+import { CircularProgress } from '@mui/material';
+import { fetchFridgesAndReports } from 'model/data';
 
-const locationBrooklynBridge = {
+const DynamicMap = dynamic(
+  () => {
+    return import('../components/organisms/browse/Map');
+  },
+  { ssr: false }
+);
+
+const defaultLocationBrooklynBridge = {
   geoLat: 40.70580857568261,
   geoLng: -73.99646699561376,
 };
+let pageData = null;
+
+const ProgressIndicator = (
+  <div
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '80vh',
+    }}
+  >
+    <CircularProgress color="secondary" />
+  </div>
+);
+const BrowseMap = (props) => <DynamicMap {...props} />;
 
 export default function BrowseMapPage() {
-  const DynamicMap = dynamic(
-    () => {
-      return import('../components/organisms/browse/Map');
-    },
-    { ssr: false }
-  );
+  const [hasDataLoaded, setHasDataLoaded] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      pageData = await fetchFridgesAndReports();
+      setHasDataLoaded(true);
+    };
+    fetchData().catch(console.error);
+  }, []);
+
   return (
     <>
       <Head>
         <title>CFM: Geographic Map</title>
       </Head>
-      <NoSsr>
-        <DynamicMap {...locationBrooklynBridge} />
-      </NoSsr>
+      {hasDataLoaded
+        ? BrowseMap({ ...defaultLocationBrooklynBridge, ...pageData })
+        : ProgressIndicator}
     </>
   );
 }
