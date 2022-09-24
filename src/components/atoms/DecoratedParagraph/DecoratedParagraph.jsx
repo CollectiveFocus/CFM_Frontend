23a185b714d6869d6f46cbb3a50d68f2d7d32166
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-09-24 13:14:27 -0700

    feat: Update DecoratedParagraph with sentence break

--- src/components/atoms/DecoratedParagraph/DecoratedParagraph.jsx
+++ src/components/atoms/DecoratedParagraph/DecoratedParagraph.jsx
@@ -12,25 +12,14 @@ export default function DecoratedParagraph({
   hasDivider = false,
   sx = {},
 }) {
-  const headingStyles = {
-    h1: {
-      mt: 12,
-      mb: 2,
-    },
-    h2: {
-      mt: 10,
-      mb: 2,
-    },
-  };
-
   const grey = designColor.grayscale.gradient[6];
 
   return (
     <Box sx={sx}>
       {hasDivider && <Divider sx={{ mt: 10, borderColor: grey }} />}
 
-      <Typography variant={variant} sx={{ ...headingStyles[variant] }}>
-        {title}
+      <Typography sx={{ ...headingStyles[variant], textAlign: 'center' }}>
+        {renderTitleSentences(title, variant)}
       </Typography>
 
       {img && (
@@ -59,3 +48,29 @@ DecoratedParagraph.propTypes = PropTypes.exact({
   hasDivider: PropTypes.bool,
   sx: PropTypes.object,
 }).isRequired;
+
+const headingStyles = {
+  h1: {
+    mt: 12,
+    mb: 2,
+  },
+  h2: {
+    mt: 10,
+    mb: 2,
+  },
+};
+
+const renderTitleSentences = (title, variant) =>
+  title
+    .split('.')
+    .filter((s) => s.length > 0)
+    .map((sentence, index) => (
+      <Typography
+        key={index}
+        variant={variant}
+        component="span"
+        sx={{ display: 'inline-block' }}
+      >
+        &nbsp;{sentence.trimStart()}.
+      </Typography>
+    ));
