c92a11c82bee41131da3caa315202799ada1e700
Bernard Martis <martisbvk@gmail.com>
2022-09-29 15:28:03 -0400

    feat: Update browse/Map organism

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,2 +1,3 @@
 export { default as DialogUpdateFridgeStatus } from './DialogUpdateFridgeStatus';
 export { default as FridgeList } from './FridgeList';
+export { default as BrowseMap } from './browse/Map';
