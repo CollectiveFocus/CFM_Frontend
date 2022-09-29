5806f523208444f66a2402ad2ae4f775fc74fec7
Bernard Martis <martisbvk@gmail.com>
2022-09-29 15:04:06 -0400

    feat: Create getFridgeList()

---
+++ src/model/view/index.js
@@ -0,0 +1,51 @@
+import { ValuesFridge, ValuesReport } from 'model/data/fridge/yup.mjs';
+
+const fridgeCache = {};
+
+export async function getFridgeList() {
+  let fridgeList = Object.values(fridgeCache);
+  if (fridgeList.length == 0) {
+    await fetchAllData();
+    fridgeList = Object.values(fridgeCache);
+  }
+  return fridgeList;
+}
+
+async function fetchAllData() {
+  const fridgeUrl = `${process.env.NEXT_PUBLIC_CFM_API_URL}/v1/fridges/`;
+  const reportsUrl = `${process.env.NEXT_PUBLIC_CFM_API_URL}/v1/reports/`;
+  const responses = await Promise.all([
+    fetch(fridgeUrl, {
+      headers: { Accept: 'application/json' },
+    }),
+    fetch(reportsUrl, {
+      headers: { Accept: 'application/json' },
+    }),
+  ]);
+  for (const response of responses) {
+    if (!response.ok) {
+      console.error(
+        `ERROR ${response.url} ${response.status}: ${response.statusText}`
+      );
+    }
+  }
+  const [fridges, reports] = await Promise.all(
+    responses.map((response) => response.json())
+  );
+  cacheAllData({ fridges, reports });
+}
+
+const castOptions = { stripUnknown: true };
+function cacheAllData({ fridges, reports }) {
+  for (const fridge of fridges) {
+    const id = fridge.id;
+    fridgeCache[id] = ValuesFridge.cast(fridge, castOptions);
+    fridgeCache[id]['report'] = null;
+  }
+  for (const report of reports) {
+    fridgeCache[report.fridgeId].report = ValuesReport.cast(
+      report,
+      castOptions
+    );
+  }
+}
