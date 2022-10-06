c19f2fed4597f18fcebb1a3a7ad9d9b7876a43c5
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-10-06 12:50:50 -0700

    feat: Create MapToggle component from ToggleButton

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,6 +1,7 @@
 export { default as ButtonLink } from './ButtonLink';
 export { default as DecoratedParagraph } from './DecoratedParagraph';
 export { default as FeedbackCard } from './FeedbackCard';
+export { default as MapToggle } from './MapToggle';
 export { default as NextLinkAnchor } from './NextLinkAnchor';
 export { default as PageFooter } from './PageFooter';
 export { default as PageHero } from './PageHero';
