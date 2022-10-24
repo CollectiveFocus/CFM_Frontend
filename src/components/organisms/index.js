35e99621ca02f13a93c55c1f3f9b9a5b1e4fef74
Bernard Martis <martisbvk@gmail.com>
2022-10-23 22:12:37 -0400

    wip: Status Report Dialog

--- src/components/organisms/index.js
+++
@@ -1,3 +0,0 @@
-export { default as DialogUpdateFridgeStatus } from './DialogUpdateFridgeStatus';
-export { default as BrowseList } from './browse/List';
-export { default as BrowseMap } from './browse/Map';
--- src/pages/user/fridge/report.jsx
+++ src/pages/user/fridge/report.jsx
@@ -1,3 +1,22 @@
+import { useState } from 'react';
+
+import { FridgeReport } from 'components/organisms/dialog/FridgeReport';
+import { FeedbackCard } from 'components/atoms';
+
 export default function FridgeReportPage() {
-  return <p>Fridge Status Report</p>;
+  const [reportSubmitted, setReportSubmitted] = useState(false);
+
+  return (
+    <div>
+      {reportSubmitted ? (
+        <FeedbackCard type={'ReportStatus'} />
+      ) : (
+        <FridgeReport
+          fridgeName="Cooper Park Community Fridge"
+          fridgeId="lescommunityfridge"
+          setReportSubmitted={setReportSubmitted}
+        />
+      )}
+    </div>
+  );
 }
