a4743b2a13605069db07bdef596f7dbbc454d6b9
Bernard Martis <martisbvk@gmail.com>
2022-11-02 12:05:11 -0400

    wip: Updated create reports for mock server settings

--- etl/src/3_load/create-reports.mjs
+++ etl/src/3_load/create-reports.mjs
@@ -3,24 +3,18 @@ import http from 'http';
 const { randomInt } = await import('crypto');
 
 const apiHost = '127.0.0.1';
-const apiPort = 3000;
-
-const httpGetOptions = {
-  hostname: apiHost,
-  port: apiPort,
-  path: '/v1/fridges',
-  method: 'GET',
-};
-
-const httpPostOptions = {
-  hostname: apiHost,
-  port: apiPort,
-  method: 'POST',
-};
+const apiPort = 3050;
 
 main();
 
 function main() {
+  const httpGetOptions = {
+    hostname: apiHost,
+    port: apiPort,
+    path: '/v1/fridges',
+    method: 'GET',
+  };
+
   const req = http.request(httpGetOptions, (res) => {
     let body = '';
 
@@ -29,6 +23,7 @@ function main() {
       try {
         let json = JSON.parse(body);
         for (let fridge of json) {
+          updatePhotoFor(fridge);
           createReportFor(fridge);
         }
       } catch (error) {
@@ -49,34 +44,52 @@ function createReportFor({ id }) {
   if (oneIn(10)) {
     return;
   }
-  const data = JSON.stringify({
+
+  const data = {
     timestamp: faker.date.recent(),
     condition: oneIn(50)
       ? 'not at location'
       : faker.helpers.arrayElement(['good', 'dirty', 'out of order', 'ghost']),
     foodPercentage: faker.helpers.arrayElement([0, 33, 67, 100]),
-    photoUrl: 'http://placekitten.com/200/300',
+    photoUrl: 'http://placekitten.com/150/100',
     notes: faker.lorem.lines(1),
-  });
-
-  httpPostOptions.path = `/v1/fridges/${id}/reports`;
-  httpPostOptions.headers = {
-    'Content-Type': 'application/json',
-    'Content-Length': data.length,
   };
 
-  const postReq = http.request(httpPostOptions, (res) => {
-    if (res.statusCode != 201) {
-      console.log(`statusCode: ${res.statusCode}`);
-    }
-  });
+  httpRequest('POST', `/v1/fridges/${id}/reports`, data)
+}
 
-  postReq.on('error', (error) => {
-    console.error(error);
-  });
-
-  postReq.write(data);
-  postReq.end();
+function updatePhotoFor(fridge) {
+  fridge.photoUrl = 'http://placekitten.com/300/345';
+  httpRequest('PATCH', `/v1/fridges/${fridge.id}`, fridge);
 }
 
 const oneIn = (n) => randomInt(n) === 0;
+
+function httpRequest(method, path, dataJson) {
+  const dataStr = JSON.stringify(dataJson);
+
+  const options = {
+    hostname: apiHost,
+    port: apiPort,
+    method,
+    path,
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': dataStr.length,
+    },
+  };
+
+  const request = http.request(options, (res) => {
+    const statusBad = res.statusCode < 200 || res.statusCode > 299
+    if (statusBad) {
+      console.log(`statusCode: ${res.statusCode}`, res.req.path);
+    }
+  });
+
+  request.on('error', (error) => {
+    console.error(error);
+  });
+
+  request.write(dataStr);
+  request.end();
+}
