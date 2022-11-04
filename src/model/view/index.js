c1ed1510b5d1304499de21afafa142ae8606f12b
Jean Paul Torre <jean.paul.ruben@gmail.com>
2022-11-03 23:19:53 -0400

    feat: Add 'hardcode api' option

--- src/model/view/index.js
+++ src/model/view/index.js
@@ -1,55 +1,55 @@
-import { ValuesFridge, ValuesReport } from 'model/data/fridge/yup.mjs';
+// import { ValuesFridge, ValuesReport } from 'model/data/fridge/yup.mjs';
 
-const fridgeCache = {};
+// const fridgeCache = {};
 
-export async function getFridgeList() {
-  let fridgeList = Object.values(fridgeCache);
-  if (fridgeList.length == 0) {
-    await fetchAllData();
-    fridgeList = Object.values(fridgeCache);
-  }
-  return fridgeList;
-}
+// export async function getFridgeList() {
+//   let fridgeList = Object.values(fridgeCache);
+//   if (fridgeList.length == 0) {
+//     await fetchAllData();
+//     fridgeList = Object.values(fridgeCache);
+//   }
+//   return fridgeList;
+// }
 
-export async function getGhostFridgeList() {
-  return fetch('data/ghostFridges.json').then((response) => response.json());
-}
+// export async function getGhostFridgeList() {
+//   return fetch('data/ghostFridges.json').then((response) => response.json());
+// }
 
-async function fetchAllData() {
-  const fridgeUrl = `https://api-prod.communityfridgefinder.com/v1/fridges/`;
-  const reportsUrl = `https://api-prod.communityfridgefinder.com/v1/reports/`;
-  const responses = await Promise.all([
-    fetch(fridgeUrl, {
-      headers: { Accept: 'application/json' },
-    }),
-    fetch(reportsUrl, {
-      headers: { Accept: 'application/json' },
-    }),
-  ]);
-  for (const response of responses) {
-    if (!response.ok) {
-      console.error(
-        `ERROR ${response.url} ${response.status}: ${response.statusText}`
-      );
-    }
-  }
-  const [fridges, reports] = await Promise.all(
-    responses.map((response) => response.json())
-  );
-  cacheAllData({ fridges, reports });
-}
+// async function fetchAllData() {
+//   const fridgeUrl = `https://api-prod.communityfridgefinder.com/v1/fridges/`;
+//   // const reportsUrl = `https://api-prod.communityfridgefinder.com/v1/reports/`;
+//   const responses = await Promise.all([
+//     fetch(fridgeUrl, {
+//       headers: { Accept: 'application/json' },
+//     })
+//     // fetch(reportsUrl, {
+//     //   headers: { Accept: 'application/json' },
+//     // }),
+//   ]);
+//   for (const response of responses) {
+//     if (!response.ok) {
+//       console.error(
+//         `ERROR ${response.url} ${response.status}: ${response.statusText}`
+//       );
+//     }
+//   }
+//   const [fridges, reports] = await Promise.all(
+//     responses.map((response) => response.json())
+//   );
+//   cacheAllData({ fridges, reports });
+// }
 
-const castOptions = { stripUnknown: true };
-function cacheAllData({ fridges, reports }) {
-  for (const fridge of fridges) {
-    const id = fridge.id;
-    fridgeCache[id] = ValuesFridge.cast(fridge, castOptions);
-    fridgeCache[id]['report'] = null;
-  }
-  for (const report of reports) {
-    fridgeCache[report.fridgeId].report = ValuesReport.cast(
-      report,
-      castOptions
-    );
-  }
-}
+// const castOptions = { stripUnknown: true };
+// function cacheAllData({ fridges, reports }) {
+//   for (const fridge of fridges) {
+//     const id = fridge.id;
+//     fridgeCache[id] = ValuesFridge.cast(fridge, castOptions);
+//     fridgeCache[id]['report'] = null;
+//   }
+//   // for (const report of reports) {
+//   //   fridgeCache[report.fridgeId].report = ValuesReport.cast(
+//   //     report,
+//   //     castOptions
+//   //   );
+//   // }
+// }
