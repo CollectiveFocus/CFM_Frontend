9ca0fec5a37c582b7652c8a6e6efa1b3b64f0dcc
Bernard Martis <martisbvk@gmail.com>
2022-11-08 00:32:07 -0500

    asset: Update content of /pamphlet/get-involved/join-a-community-group/ page

--- src/pages/pamphlet/get-involved/join-a-community-group.jsx
+++ src/pages/pamphlet/get-involved/join-a-community-group.jsx
@@ -1,89 +1,94 @@
-import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { PageFooter, PamphletParagraph } from 'components/atoms';
-import { Typography, Box } from '@mui/material';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
+import { PageHero, PamphletParagraph, PageFooter } from 'components/atoms';
 
-export async function getStaticProps() {
-  return {
-    props: {
-      organizations: [
-        { name: 'Collective Focus Resource Hub' },
-        { name: 'One Love Community Fridge' },
-        { name: 'Black Chef Movement' },
-        { name: 'Stuff4Good' },
-        { name: 'Artists.Athletes.Activists' },
-        { name: 'Universe City' },
-        { name: 'Woodbine Mutual Aid' },
-        { name: 'Nuestra Mesa Brooklyn' },
+const pageContent = {
+  pageHero: {
+    img: {
+      src: '/hero/join_a_community_group.webp',
+      alt: 'Volunteers resting',
+    },
+  },
+  content: [
+    {
+      title: 'Join a community group',
+      variant: 'h1',
+      body: [
+        'The fridges near you most likely needs help cleaning and sourcing food. Anyone can participate in running community fridges. You are welcome to organize your own initiatives because operations for community fridges are decentralized and autonomous.',
+        'On Fridge Finder, you can find a fridge near you, connect with the location, and share status updates. To collaborate with us behind the scenes, join our engineering or outreach teams.',
+      ],
+      button: {
+        title: 'Volunteer',
+        to: {
+          pathname: '/user/contact',
+          query: { subject: 'Volunteer Interest' },
+        },
+        'aria-label': 'Volunteer',
+        variant: 'contained',
+      },
+    },
+    {
+      variant: 'h2',
+      title: 'Community groups we recommend',
+      body: [
+        'There are groups across New York City that source donations and maintenance support for fridges. This is essential to keeping fridges active.',
+        'The following organizations have initiatives to support community fridges. To participate, contact the groups that interest you.',
       ],
     },
-  };
-}
+  ],
+};
+const organizations = [
+  {
+    name: 'Artists.Athletes.Activists',
+    url: 'https://artists-athletes-activists.org/',
+  },
+  { name: 'Black Chef Movement', url: 'https://blackchefmovement.org/' },
+  {
+    name: 'Black Voices Matter',
+    url: 'https://www.blackvoicesmatterpledge.org/',
+  },
+  { name: 'Bushwick Ayuda Mutua ', url: 'https://bushwickayudamutua.com/' },
+  {
+    name: 'Collective Focus Resource Hub',
+    url: 'https://collectivefocus.site/',
+  },
+  { name: 'Freedge', url: 'https://freedge.org/' },
+  { name: 'Nuestra Mesa Brooklyn', url: 'https://www.nuestramesabk.com/' },
+  {
+    name: 'One Love Community Fridge',
+    url: 'https://www.onelovecommunityfridge.org/',
+  },
+  { name: 'Stuff4Good', url: 'https://officialgoodstuff.com/stuff4good/' },
+  { name: 'Universe City', url: 'https://www.universecity.nyc/' },
+  { name: 'Woodbine Mutual Aid', url: 'https://www.woodbine.nyc/mutualaid/' },
+];
 
-export default function JoinACommunityGroupPage({ organizations }) {
+export default function JoinACommunityGroupPage() {
+  const { pageHero, content } = pageContent;
   return (
     <>
       <Head>
-        <title>Fridge Finder: Join a Community Group</title>
+        <title>Fridge Finder: Join a community group</title>
       </Head>
-      <Box mx={8} mb={6}>
+      <PageHero {...pageHero} />
+
+      {content.map((paragraph, index) => (
         <PamphletParagraph
-          title="Join a Community Group."
-          variant="h1"
-          body={[
-            'The fridges near you most likely needs help cleaning and sourcing food. Anyone can participate in running community fridges. You are welcome to organize your own initiatives because operations for community fridges are decentralized and autonomous.',
-            'On Fridge Finder, you can find a fridge near you, connect with the location, and share status updates. To collaborate with us behind the scenes, join our engineering or outreach teams.',
-          ]}
-          button={{
-            title: 'Volunteer with Collective Focus',
-            to: {
-              pathname: '/user/contact',
-              query: { subject: 'Volunteer Interest' },
-            },
-            'aria-label': 'Volunteer with Collective Focus',
-            variant: 'contained',
-            size: 'wide',
-          }}
+          {...paragraph}
+          key={index + '_JoinACommunityGroupPage'}
+          hasDivider={index > 0}
         />
-
-        <Typography sx={{ mb: 7 }} variant="body1">
-          There are groups across New York City that source donations and
-          maintenance support for fridges. This is essential to keeping fridges
-          active.
-        </Typography>
-
-        <PamphletParagraph
-          variant="h2"
-          title="Community Groups We Recommend"
-          body={[
-            'The following organizations have initiatives to support community fridges. To participate, contact the groups that interest you.',
-          ]}
-        />
-
-        {renderOrganizations(organizations)}
-      </Box>
-
-      <PageFooter fixedAtBottom={false} />
-    </>
-  );
-}
-JoinACommunityGroupPage.propTypes = {
-  organizations: PropTypes.arrayOf(PropTypes.exact({ name: PropTypes.string })),
-};
-
-function renderOrganizations(orgs) {
-  //todo add better styling
-
-  return (
-    <List>
-      {orgs.map((org) => (
-        <ListItem key={org.name} sx={{ py: 1 }}>
-          <ListItemText>{org.name}</ListItemText>
-        </ListItem>
       ))}
-    </List>
+      <ul style={{ margin: '0 2em' }}>
+        {organizations.map(({ name, url }, index) => (
+          <li key={index + '_org'}>
+            <a href={url} target="_blank" rel="noreferrer">
+              {name}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <PageFooter fixedAtBottom={true} />
+    </>
   );
 }
