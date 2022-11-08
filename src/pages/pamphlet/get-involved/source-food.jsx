8d48bead7d92b37c1e1fd043fe2f2391ad4a3e4e
Bernard Martis <martisbvk@gmail.com>
2022-11-08 00:32:05 -0500

    asset: Update content of /pamphlet/get-involved/source-food/ page

--- src/pages/pamphlet/get-involved/source-food.jsx
+++ src/pages/pamphlet/get-involved/source-food.jsx
@@ -1,32 +1,51 @@
 import Head from 'next/head';
-import { Box } from '@mui/material';
-import { PageFooter, PamphletParagraph } from 'components/atoms';
+import { PageHero, PamphletParagraph, PageFooter } from 'components/atoms';
 
-export default function SourceFood() {
+const pageContent = {
+  pageHero: {
+    img: {
+      src: '/hero/source_food.webp',
+      alt: 'Boxes of food stacked up in front of Community Focus',
+    },
+  },
+  content: [
+    {
+      variant: 'h1',
+      title: 'Source Food',
+      body: [
+        'Sourcing food donations is possible by building relationships with businesses that have surplus products. Fridge organizers are able to redirect food waste from bakeries, grocery stores, pantries, cafes,restaurants, and more. That way, perfectly good food can provide nutrition to people in need, instead of being thrown away. Businesses have many incentives to partner with community fridges. Excess food causes a negative environmental impact and inefficiencies within our economy, which community fridges can help resolve. The best way to approach a business about donating food would be to present materials about community fridges and create a proposal.',
+      ],
+      button: {
+        title: 'Source Food',
+        to: {
+          pathname: '/user/contact',
+          query: { subject: 'Sourcing Food Inquiry' },
+        },
+        'aria-label': 'Sourcing Food Inquiry',
+        variant: 'contained',
+        size: 'wide',
+      },
+    },
+  ],
+};
+
+export default function SourceFoodPage() {
+  const { pageHero, content } = pageContent;
   return (
     <>
       <Head>
         <title>Fridge Finder: Source Food</title>
       </Head>
-      <Box mx={8} mb={6}>
+
+      <PageHero {...pageHero} />
+
+      {content.map((paragraph, index) => (
         <PamphletParagraph
-          variant="h1"
-          title="Source Food"
-          body={[
-            'Sourcing food donations is possible by building relationships with businesses that have surplus products. Fridge organizers are able to redirect food waste from bakeries, grocery stores, pantries, cafes,restaurants, and more. That way, perfectly good food can provide nutrition to people in need, instead of being thrown away. Businesses have many incentives to partner with community fridges. Excess food causes a negative environmental impact and inefficiencies within our economy, which community fridges can help resolve. The best way to approach a business about donating food would be to present materials about community fridges and create a proposal.',
-          ]}
-          button={{
-            title: 'Source Food',
-            to: {
-              pathname: '/user/contact',
-              query: { subject: 'Sourcing Food Inquiry' },
-            },
-            'aria-label': 'Sourcing Food Inquiry',
-            variant: 'contained',
-            size: 'wide',
-          }}
+          {...paragraph}
+          key={index + '_SourceFoodPage'}
+          hasDivider={index > 0}
         />
-      </Box>
+      ))}
 
       <PageFooter fixedAtBottom={true} />
     </>
