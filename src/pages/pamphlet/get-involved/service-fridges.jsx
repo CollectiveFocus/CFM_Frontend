ac9498998f0e657daaa57d9c14fe13e3cf8e4ef0
Bernard Martis <martisbvk@gmail.com>
2022-11-08 00:32:06 -0500

    asset: Update content of /pamphlet/get-involved/service-fridges/ page

--- src/pages/pamphlet/get-involved/service-fridges.jsx
+++ src/pages/pamphlet/get-involved/service-fridges.jsx
@@ -1,32 +1,50 @@
 import Head from 'next/head';
-import { Box } from '@mui/material';
-import { PageFooter, PamphletParagraph } from 'components/atoms';
+import { PageHero, PamphletParagraph, PageFooter } from 'components/atoms';
 
-export default function ServiceFridges() {
+const pageContent = {
+  pageHero: {
+    img: {
+      src: '/hero/service_fridges.webp',
+      alt: 'Man inspecting fridge',
+    },
+  },
+  content: [
+    {
+      variant: 'h1',
+      title: 'Taking care of fridges',
+      body: [
+        'Fridges should be cleaned daily. If you see trash at a fridge location, take the trash with you to dispose of it properly. If the fridge is in need of a repair, you can alert our community on Fridge Finder by submitting a status report for that fridge. Do you have repair skills that can fix broken refrigerators? Can you help build fridge shelters? Contact us.',
+      ],
+      button: {
+        title: 'Service Fridges',
+        to: {
+          pathname: '/user/contact',
+          query: { subject: 'Fridge Service Interest' },
+        },
+        'aria-label': 'Interested in fixing and maintaining a fridge',
+        variant: 'contained',
+      },
+    },
+  ],
+};
+
+export default function ServiceFridgesPage() {
+  const { pageHero, content } = pageContent;
   return (
     <>
       <Head>
-        <title>Fridge Finder: Service Fridges</title>
+        <title>Fridge Finder: Service fridges</title>
       </Head>
-      <Box mx={8} mb={6}>
+
+      <PageHero {...pageHero} />
+
+      {content.map((paragraph, index) => (
         <PamphletParagraph
-          title="Taking Care of Fridges"
-          variant="h1"
-          body={[
-            'Fridges should be cleaned daily. If you see trash at a fridge location, take the trash with you to dispose of it properly. If the fridge is in need of a repair, you can alert our community on Fridge Finder by submitting a status report for that fridge. Do you have repair skills that can fix broken refrigerators? Can you help build fridge shelters? Contact us.',
-          ]}
-          button={{
-            title: 'Service Fridges',
-            to: {
-              pathname: '/user/contact',
-              query: { subject: 'Fridge Service Interest' },
-            },
-            'aria-label': 'Interested in fixing and maintaining a fridge',
-            variant: 'contained',
-            size: 'wide',
-          }}
+          {...paragraph}
+          key={index + '_ServiceFridgesPage'}
+          hasDivider={index > 0}
         />
-      </Box>
+      ))}
 
       <PageFooter fixedAtBottom={true} />
     </>
