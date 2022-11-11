08dbfb0598b67be3a738aa77f1e0dc2bf8e2f528
Bernard Martis <martisbvk@gmail.com>
2022-11-11 00:30:54 -0500

    feat: Update model/data/fridge/PropTypes to export js objects

--- src/model/data/fridge/prop-types.js
+++ src/model/data/fridge/prop-types.js
@@ -1,9 +1,8 @@
 import PropTypes from 'prop-types';
 
-export const typesTag = PropTypes.string.isRequired;
-export const typesTags = PropTypes.arrayOf(typesTag);
+export const typeTag = PropTypes.string.isRequired;
 
-export const typesLocation = PropTypes.exact({
+export const fieldsLocation = {
   name: PropTypes.string,
   street: PropTypes.string.isRequired,
   city: PropTypes.string.isRequired,
@@ -11,53 +10,41 @@ export const typesLocation = PropTypes.exact({
   zip: PropTypes.string.isRequired,
   geoLat: PropTypes.number.isRequired,
   geoLng: PropTypes.number.isRequired,
-});
+};
 
-export const typesMaintainer = PropTypes.exact({
+export const fieldsMaintainer = {
   name: PropTypes.string,
   email: PropTypes.string,
   organization: PropTypes.string,
   phone: PropTypes.string,
   website: PropTypes.string,
   instagram: PropTypes.string,
-});
+};
 
-export const typesFridge = PropTypes.exact({
+export const fieldsFridge = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  location: typesLocation.isRequired,
-  tags: typesTags,
-  maintainer: typesMaintainer,
+  location: PropTypes.exact(fieldsLocation).isRequired,
+  tags: PropTypes.arrayOf(typeTag),
+  maintainer: PropTypes.exact(fieldsMaintainer),
   photoUrl: PropTypes.string,
   notes: PropTypes.string,
   verified: PropTypes.bool,
-});
+};
 
-export const typesReportCondition = PropTypes.oneOf([
+export const typeCondition = PropTypes.oneOf([
   'good',
   'dirty',
   'out of order',
   'not at location',
   'ghost',
 ]);
-export const typesReportFoodPercentage = PropTypes.oneOf([0, 1, 2, 3]);
+export const typeFoodPercentage = PropTypes.oneOf([0, 1, 2, 3]);
 
-export const typesReport = PropTypes.exact({
-  timestamp: PropTypes.string.isRequired,
-  condition: typesReportCondition.isRequired,
-  foodPercentage: typesReportFoodPercentage.isRequired,
+export const fieldsReport = {
+  timestamp: PropTypes.object.isRequired,
+  condition: typeCondition.isRequired,
+  foodPercentage: typeFoodPercentage.isRequired,
   photoUrl: PropTypes.string,
   notes: PropTypes.string,
-});
-
-const typesDataFridge = {
-  Fridge: typesFridge,
-  Location: typesLocation,
-  Maintainer: typesMaintainer,
-  Report: typesReport,
-  ReportCondition: typesReportCondition,
-  ReportFoodPercentage: typesReportFoodPercentage,
-  Tag: typesTag,
-  Tags: typesTags,
 };
-export default typesDataFridge;
