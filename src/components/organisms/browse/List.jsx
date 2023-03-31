b4053d2d38746616e5589dafb15fc57ea267b838
Youssef El Rhiassi <azerty7-@live.fr>
2023-03-31 23:06:22 +0200

    fix: Styles for item's address

--- src/components/organisms/browse/List.jsx
+++ src/components/organisms/browse/List.jsx
@@ -12,9 +12,22 @@ import {
 
 import typesView from 'model/view/prop-types';
 
-function Location({ location }) {
+function Location({ location, updateMarker = () => {} }) {
   return (
-    <Stack direction="row" spacing={3} alignItems="center">
+    <Stack
+      direction="row"
+      spacing={3}
+      alignItems="center"
+      sx={{
+        backgroundColor: '#8484841c',
+        padding: '0.5rem',
+        borderStyle: 'none',
+        boxShadow: '0px 0px 9px 3px #8484841c',
+        borderRadius: '7px',
+        cursor: 'pointer',
+      }}
+      onClick={() => updateMarker(location)}
+    >
       <LocationOnOutlinedIcon />
       <Typography sx={{ fontSize: ['0.9375rem'], color: 'text.primary' }}>
         {`${location.street} ${location.city}, ${location.state} ${location.zip}`}
@@ -24,6 +37,7 @@ function Location({ location }) {
 }
 Location.propTypes = {
   location: typesView.Location,
+  updateMarker: PropTypes.func,
 };
 
 function Instagram({ instagramUrl }) {
@@ -66,7 +80,6 @@ export default function FridgeList({ fridges, setCurrentMarker }) {
       {fridges.map((fridge, fridgeIndex) => (
         <ListItem
           key={fridge.id}
-          onClick={() => updateMarker(fridge.location)}
           divider={fridgeIndex !== fridges.length - 1}
           sx={{ paddingY: 5.5, paddingX: 0 }}
         >
@@ -76,7 +89,10 @@ export default function FridgeList({ fridges, setCurrentMarker }) {
                 <Typography sx={{ fontSize: ['1rem'], fontWeight: 700 }}>
                   {fridge.name}
                 </Typography>
-                <Location location={fridge.location} />
+                <Location
+                  location={fridge.location}
+                  updateMarker={updateMarker}
+                />
                 {fridge.maintainer?.instagram ? (
                   <Instagram instagramUrl={fridge.maintainer.instagram} />
                 ) : null}
