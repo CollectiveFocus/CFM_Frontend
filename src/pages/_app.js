aad7f8f32cdfe58bed526d56599c0b126147d831
Sean Redmon <seanredmon@seanred.io>
2023-05-05 18:13:08 -0400

    feat: Create Google Analytics integration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { CacheProvider } from '@emotion/react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
@@ -7,6 +10,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import AppBar from 'components/molecules/AppBar';
 
 import createEmotionCache from 'lib/createEmotionCache';
+import GoogleAnalytics from 'lib/analytics';
 import theme from 'theme';
 
 const clientSideEmotionCache = createEmotionCache();
@@ -16,11 +20,40 @@ export default function MyApp({
   emotionCache = clientSideEmotionCache,
   pageProps,
 }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      GoogleAnalytics.view(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GoogleAnalytics.TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GoogleAnalytics.TRACKING_ID}`}
+      />
       <CacheProvider value={emotionCache}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
