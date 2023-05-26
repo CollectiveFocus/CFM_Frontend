0e9996af2c3c32c8e62538e4cc6887dfe5b0c128
Bernard Martis <martisbvk@gmail.com>
2023-05-26 01:10:32 -0400

    wip: Code review

--- src/components/atoms/ButtonLink/ButtonLink.test.jsx
+++ src/components/atoms/ButtonLink/ButtonLink.test.jsx
@@ -1,23 +1,17 @@
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 import ButtonLink from './ButtonLink';
 
 describe('ButtonLink', () => {
-  const defaultProps = {
-    to: '/about',
-    'aria-label': 'Learn more',
-    variant: 'outlined',
-  };
-
-  const getRender = (props = defaultProps) => render(<ButtonLink {...props} />);
+  const renderButtonLink = (props) => render(<ButtonLink {...props} />);
 
   it('should render the correct props', () => {
-    const { getByRole } = getRender();
-
-    const button = getByRole('button');
-    console.log('🚀 ~ file: ButtonLink.test.jsx:17 ~ it ~ button:', button);
+    const button = renderButtonLink({
+      to: '/about',
+      'aria-label': 'Learn more',
+      variant: 'outlined',
+    }).getByRole('link');
     expect(button).toHaveAttribute('href', '/about');
     expect(button).toHaveAttribute('aria-label', 'Learn more');
-    expect(button).toHaveClass('MuiButton-contained');
+    expect(button).toHaveClass('MuiButton-outlined');
   });
 });
