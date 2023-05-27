6d43b8ffaaa5611ee0c53e5e3767768e083d32a6
Ricky Saka <sakaricky91@gmail.com>
2023-05-27 18:59:38 +0100

    feat: Add test for ButtonLink

--- src/components/atoms/ButtonLink/ButtonLink.test.jsx
+++ src/components/atoms/ButtonLink/ButtonLink.test.jsx
@@ -1,8 +1,24 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import ButtonLink from './ButtonLink';
 
 describe('ButtonLink', () => {
   const renderButtonLink = (props) => render(<ButtonLink {...props} />);
+
+  jest.mock(
+    'next/link',
+    () =>
+      ({ children }) =>
+        children
+  );
+
+  it('should render the ButtonLink component correctly', () => {
+    const button = renderButtonLink({
+      to: '/about',
+      'aria-label': 'Learn more',
+      variant: 'outlined',
+    }).getByRole('link');
+    expect(button).toMatchSnapshot();
+  });
 
   it('should render the correct props', () => {
     const button = renderButtonLink({
@@ -10,6 +26,7 @@ describe('ButtonLink', () => {
       'aria-label': 'Learn more',
       variant: 'outlined',
     }).getByRole('link');
+
     expect(button).toHaveAttribute('href', '/about');
     expect(button).toHaveAttribute('aria-label', 'Learn more');
     expect(button).toHaveClass('MuiButton-outlined');
