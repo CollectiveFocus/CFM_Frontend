2b7546e176b8538073e363a53529ed93ad20408a
Youssef El Rhiassi <azerty7-@live.fr>
2023-07-06 14:33:44 +0200

    test: UI test for PamphletParagraph

---
+++ src/components/atoms/PamphletParagraph/pamphletParagraph.test.js
@@ -0,0 +1,69 @@
+import { getByRole, render } from '@testing-library/react';
+import PamphletParagraph from './PamphletParagraph';
+
+const mockTitle = 'Mock Title';
+const mockVariant = 'h2';
+const mockImg = {
+  src: '/paragraph/pamphlet/about/independence_for_each_fridge.webp',
+  alt: 'Mock image',
+  width: 414,
+  height: 276,
+};
+const mockBody = ['Paragraph 1', 'Paragraph 2'];
+const mockButton = {
+  variant: 'contained',
+  to: '/path',
+  'aria-label': 'Button',
+  title: 'Click Me',
+};
+
+describe('PamphletParagraph', () => {
+  it('renders correctly with all props', () => {
+    const { getByText, getByAltText } = render(
+      <PamphletParagraph
+        title={mockTitle}
+        variant={mockVariant}
+        img={mockImg}
+        body={mockBody}
+        button={mockButton}
+        hasDivider={true}
+      />
+    );
+
+    // the title is rendered with the correct text and variant
+    expect(getByText('Mock Title')).toBeInTheDocument();
+    expect(getByText('Mock Title')).toHaveProperty('tagName', 'H2');
+
+    //  the image is rendered with the correct alt text
+    expect(getByAltText('Mock image')).toBeInTheDocument();
+
+    // the paragraphs are rendered with the correct text
+    expect(getByText('Paragraph 1')).toBeInTheDocument();
+    expect(getByText('Paragraph 2')).toBeInTheDocument();
+
+    // the button is rendered with the correct text, variant, and aria-label
+    const button = getByText('Click Me');
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveAttribute('aria-label', 'Button');
+  });
+
+  it('renders correctly without optional props', () => {
+    const { queryByAltText, queryByText, getByText } = render(
+      <PamphletParagraph title={mockTitle} variant={mockVariant} />
+    );
+
+    // the title is rendered with the correct text and variant
+    expect(getByText('Mock Title')).toBeInTheDocument();
+    // expect(getByText('Mock Title')).toHaveAttribute('variant', 'h2');
+
+    // the image is not rendered
+    expect(queryByAltText('Mock image')).toBeNull();
+
+    // the paragraphs are not rendered
+    expect(queryByText('Paragraph 1')).toBeNull();
+    expect(queryByText('Paragraph 2')).toBeNull();
+
+    //  the button is not rendered
+    expect(queryByText('Click Me')).toBeNull();
+  });
+});
