fbece3d614ad42933ca41f54d04fbc8f41ec2fb4
Bernard Martis <martisbvk@gmail.com>
2025-09-15 22:11:16 -0400

    asset: Update content of /pamphlet/best-practices/ page

--- src/pages/pamphlet/best-practices.page.jsx
+++ src/pages/pamphlet/best-practices.page.jsx
@@ -2,42 +2,34 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { Box, Tab, Tabs, Typography } from '@mui/material';
-import { PageHero, PageFooter } from 'components/atoms';
+import { PageFooter } from 'components/atoms';
 
-const pageContent = {
-  pageHero: {
-    img: {
-      src: '/hero/best_practices.webp',
-      alt: 'Donor placing food into a fridge',
-    },
+const panelList = [
+  {
+    title: 'Dropping off',
+    content: [
+      'Only bring good food to the community fridges. When considering what is good to donate, ask yourself if you would give the food item to your friends or family to eat? If so, your food donation is probably good for your neighbors, too.',
+      'Food must be fresh, stored at the proper temperature, and unexpired.',
+      'Portion donations into individual sized quantities that make it easy for people to take with them. Catering trays should not be stored in community fridges due to causing food contents to spill, and being inaccessible for the public to transport.',
+      'Food should be kept in clean, airtight containers to avoid food spills.',
+      'Label meals with ingredients and the date prepared.',
+      'If you notice a fridge needs to be cleaned, help clean it.',
+      'Do not bring anything else that is not food to a community fridge, unless told otherwise.',
+      'Take your trash with you, including cardboard boxes and food scraps.',
+    ],
   },
-  panelList: [
-    {
-      title: 'Dropping off',
-      content: [
-        'Only bring good food to the community fridges. When considering what is good to donate, ask yourself if you would give the food item to your friends or family to eat? If so, your food donation is probably good for your neighbors, too.',
-        'Food must be fresh, stored at the proper temperature, and unexpired.',
-        'Portion donations into individual sized quantities that make it easy for people to take with them. Catering trays should not be stored in community fridges due to causing food contents to spill, and being inaccessible for the public to transport.',
-        'Food should be kept in clean, airtight containers to avoid food spills.',
-        'Label meals with ingredients and the date prepared.',
-        'If you notice a fridge needs to be cleaned, help clean it.',
-        'Do not bring anything else that is not food to a community fridge, unless told otherwise.',
-        'Take your trash with you, including cardboard boxes and food scraps.',
-      ],
-    },
-    {
-      title: 'Picking up',
-      content: [
-        'Only take the amount of food that you need, and leave the rest for others. Many people depend on community fridges to get enough nutrition. Whenever possible, make sure there is enough food left for others to eat as well.',
-        'Only take good food from the fridges. If the food does not look good, throw it away using appropriate procedures and sanitation.',
-        'If you touch a food item, take it with you or throw it away.',
-        'Do not leave trash near community fridges.',
-        'If you notice a fridge needs to be cleaned, help clean it.',
-        'Be kind to others when interacting with community fridges.',
-      ],
-    },
-  ],
-};
+  {
+    title: 'Picking up',
+    content: [
+      'Only take the amount of food that you need, and leave the rest for others. Many people depend on community fridges to get enough nutrition. Whenever possible, make sure there is enough food left for others to eat as well.',
+      'Only take good food from the fridges. If the food does not look good, throw it away using appropriate procedures and sanitation.',
+      'If you touch a food item, take it with you or throw it away.',
+      'Do not leave trash near community fridges.',
+      'If you notice a fridge needs to be cleaned, help clean it.',
+      'Be kind to others when interacting with community fridges.',
+    ],
+  },
+];
 
 function TabPanel(props) {
   const { children, currentTab, index, ...other } = props;
@@ -72,7 +64,6 @@ TabPanel.propTypes = {
 };
 
 export default function BestPracticesPage() {
-  const { pageHero, panelList } = pageContent;
   const [ixCurrentPanel, setCurrentPanelIndex] = useState(0);
 
   const handleChange = (event, newValue) => {
@@ -87,7 +78,6 @@ export default function BestPracticesPage() {
       <Typography sx={{ ml: 4 }} variant="h1">
         Best Practices
       </Typography>
-      <PageHero {...pageHero} />
       <Tabs
         sx={{ mx: 4 }}
         value={ixCurrentPanel}
