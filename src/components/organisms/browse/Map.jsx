e6ee13e2b3ee0e591431f95fceed69563a933c99
Bernard Martis <martisbvk@gmail.com>
2025-10-02 20:39:12 -0400

    style: Format code using prettier@3.6 and svgo@4.0 configuration

--- src/components/organisms/browse/Map.jsx
+++ src/components/organisms/browse/Map.jsx
@@ -71,7 +71,7 @@ export default function Map({ fridgeList }) {
       >
         <UpdateCenter fridgeList={fridgeList} />
         <TileLayer
-          attribution='&copy; Fridge Finder'
+          attribution="&copy; Fridge Finder"
           url="https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
           maxZoom={19}
           subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
