489129a980f4b5af72971ae220e4fd6c80160e1e
Briana Calderón Navarro <globalhandsagency@gmail.com>
2025-11-25 17:56:14 -0500

    asset: Update content of /pamphlet/about/ page

--- src/pages/pamphlet/about.page.jsx
+++ src/pages/pamphlet/about.page.jsx
@@ -1,99 +1,83 @@
 import Head from 'next/head';
-import { PageHero, PamphletParagraph, PageFooter } from 'components/atoms';
+import { PamphletParagraph, PageFooter } from 'components/atoms';
 
 const pageContent = {
-  pageHero: {
-    img: {
-      src: '/hero/about.webp',
-      alt: 'The group at Community Focus',
-    },
-  },
   content: [
     {
       variant: 'h1',
-      title: 'How it Started',
+      title: 'Summary',
       body: [
-        'Community fridges became popular in 2020 as a way to directly provide free food to people in need and combat food waste. During the pandemic, outdoor refrigerators served as emergency access points for life-sustaining nutrition in dozens of cities around the world. The food found inside fridges come from diverse sources, mostly individual donors or surplus food that is “rescued” from the supply chain.',
+        'Community fridges provide free food to people in need while combating food waste. Outdoor refrigerators serve as access points to life-sustaining nutrition in cities around the world.',
       ],
     },
     {
       img: {
         src: '/paragraph/pamphlet/about/independence_for_each_fridge.webp',
-        alt: 'People making their own choices',
-        width: 414,
-        height: 276,
+        alt: 'A man in working clothes stands in the hot sun painting the doors of a fridge blue. The a sign with the words "Free Food" hangs on the lintel of the fridge shed.',
+        attribution: 'Mr. Moda painting Classon Community Fridge, 2025',
       },
       variant: 'h2',
       title: 'Independence for Each Fridge',
       body: [
-        'Community fridges are independently operated by local businesses, activists, neighbors, or faith-based organizations. Cumulatively, fridge hosts and organizers do not identify with a singular monolithic identity or ethos. Instead, fridges are as unique as the neighborhoods that have them. Community fridges are used by all different types of people everyday, oftentimes working class families, immigrants, and homeless populations in need of an extra food source. A common value is respect for the autonomy of each refrigerator.',
+        'Community fridges are used by many people everyday, oftentimes working class families, the elderly, and unhoused people looking for an extra food source. A common value is respect for the autonomy of each refrigerator and those who depend on them.',
+        'The thousands of fridges in action represent a variety of identities and ethos that are as unique as the neighborhoods that run them. Refrigerators are independent and decentralized. They are operated by local businesses, mutual aid groups, neighbors, and organizations.',
       ],
     },
     {
-      img: {
-        src: '/paragraph/pamphlet/about/public_art_installation_on_our_sidewalks.webp',
-        alt: 'Artists painting',
-        width: 414,
-        height: 276,
-      },
       variant: 'h2',
-      title: 'A Public Art Installation on Our Sidewalks',
+      title: 'Public Art on Our Sidewalks',
       body: [
-        'Artists popularized the community fridges by painting colorful designs on the doors and sides to attract interest and curiosity. This creative element effectively transforms an everyday object into a cultural artifact that carries a powerful message about circular economics and mutual aid. As public art installations, the fridges receive widespread support from the press and social media, giving visibility to this project’s social impact.',
+        'The first known community fridge was started in 2014. Since then, artists popularized these public resources by decorating the fridge doors to attract curiosity and welcome participants. This creative element effectively transforms an everyday object into a cultural icon that carries a powerful message about circular economics and mutual aid. As public art installations, fridges generate interest and expand the positive impact of food redistribution.',
       ],
     },
     {
       img: {
         src: '/paragraph/pamphlet/about/technology_empowers_us.webp',
-        alt: 'Technology resources',
-        width: 414,
-        height: 276,
+        alt: 'A table  on top of which stands a cardboard cutout of the Fridge Finder mascots. On top of the table are printed booklets, a welcome poster, and an iPad on a display stand.',
+        attribution: 'FridgeFinder table display, 2025',
       },
       variant: 'h2',
       title: 'Technology Empowers Us',
       body: [
-        'Taking our mission a step further, a small team of artists and engineers united to build this fridge app. We are Collective Focus, an organization that provides resources and creative opportunities. The goal of our app is to support a larger consortium of people working together to maintain fridges in New York City. By using technology for good, we are building greater capacity for this project to prosper long term. With this app, people are empowered to check on fridges with more organizational capacity, volunteer drivers are mobilized to transport rescued food to fridges, and the incredible everyday contributions to this work are archived for future inspiration',
+        'A team of organizers and web developers formed in 2022 to create FridgeFinder, a map that tracks community fridges. Our app offers digital tools that empower volunteers to take action. By subscribing to a fridge and leaving status updates, the public strengthens a network of people running fridges. Maintenance is also streamlined and food transportation is mobilized. FridgeFinder shows the incredible everyday contributions of food justice advocates. Our goal as a team is to strengthen the capacity for long-term prosperity at featured locations.',
+      ],
+    },
+    {
+      variant: 'h3',
+      title: 'Web App',
+      body: [
+        'On our website everyone can access an interactive map and make status updates.',
+      ],
+    },
+    {
+      variant: 'h3',
+      title: 'Mobile App',
+      body: [
+        'Make a user profile, subscribe to fridges, and upload photos by downloading the mobile app.',
       ],
     },
     {
       img: {
-        src: '/paragraph/pamphlet/about/collective_focus.webp',
-        alt: 'Volunteers standing in front of the Collective Focus building',
-        width: 414,
-        height: 276,
+        src: '/paragraph/pamphlet/about/support_the_fridges.webp',
+        alt: 'Volunteers loading plastic bags containing groceries into the trunk of a SUV.',
+        attribution: 'Food donation pick up, 2025',
       },
       variant: 'h2',
-      title: 'Collective Focus',
+      title: 'Support the Fridges',
       body: [
-        'Collective Focus is an artist community that builds prosperity through resource distribution and creative opportunities. Our team has launched, maintained, and operated community fridges for over 2 years, making an impact on 50 fridges worldwide. We currently host four fridges at our physical location in Brooklyn and we provide support to grassroots organizers within our network.',
+        'The growth and success of this project depends on your contributions. There are many ways to help combat food injustice. Volunteer opportunities are available with FridgeFinder, as well as with individual refrigerators actively recruiting new collaborators.',
       ],
-    },
-    {
-      variant: 'h2',
-      title: 'Help out the fridges',
-      body: [
-        'The growth and success of the project depends on community support. There are many ways to help out the community fridge movement in New York and beyond.',
-      ],
-      button: {
-        title: 'Get Involved',
-        to: '/pamphlet/get-involved',
-        'aria-label': 'Get involved with the community fridge movement',
-        variant: 'contained',
-        size: 'wide',
-      },
     },
   ],
 };
 
 export default function AboutPage() {
-  const { pageHero, content } = pageContent;
+  const { content } = pageContent;
   return (
     <>
       <Head>
         <title>Fridge Finder: About Us</title>
       </Head>
-      <PageHero {...pageHero} />
-
       {content.map((paragraph, index) => (
         <PamphletParagraph
           {...paragraph}
